c575f4a7ea77a22275c0d952e6bc2b4c8cc50e18
Steveen <steveen.echeverri@gmail.com>
2020-10-02 15:38:03 -0500

    Hash password with bcrypt

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
 
 router.get("/test", (req, res) => {
@@ -7,7 +8,7 @@ router.get("/test", (req, res) => {
 
 router.post("/register", async (req, res) => {
     try {
-        const { email, password, passwordCheck, displayName } = req.body;
+        let { email, password, passwordCheck, displayName } = req.body;
 
         if (!email || !password || !passwordCheck)
             return res.status(400).json({ msg: "Not all field have been entered." });
@@ -20,15 +21,26 @@ router.post("/register", async (req, res) => {
                 .status(400)
                 .json({ msg: "Enter the same password twice for verification." });
 
-        const existingUser = await User.find({ email: email });
+        const existingUser = await User.findOne({ email: email });
         if (existingUser)
             return res
                 .status(400)
                 .json({ msg: "An accont with this email already exists." });
 
         if (!displayName) displayName = email;
+
+        const salt = await bcrypt.genSaltSync(10);
+        const passwordHast = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            email,
+            password: passwordHast,
+            displayName
+        });
+        const savedUser = await newUser.save();
+        res.json(savedUser);
     } catch (err) {
-        res.status(500).json({ err });
+        res.status(500).json({ error: err.message });
     }
 });
 
