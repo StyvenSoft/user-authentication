8184c82d24d7a8728f142d546ecb0b83261d21d9
Steveen <steveen.echeverri@gmail.com>
2020-10-01 19:55:32 -0500

    Register user route and validation

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,4 +4,19 @@ router.get("/test", (req, res) => {
     res.send("Working..");
 });
 
+router.post("/register", async (req, res) => {
+    const { email, password, passwordCheck, displayName } = req.body;
+
+    if (!email || !password || !passwordCheck)
+        return res.status(400).json({ msg: "Not all field have been entered." });
+    if (password.length < 5)
+        return res
+            .status(400)
+            .json({ msg: "The password needs to be at least 5 character long." });
+    if (password !== passwordCheck)
+        return res
+            .status(400)
+            .json({ msg: "Enter the same password twice for verification." })
+});
+
 module.exports = router;
