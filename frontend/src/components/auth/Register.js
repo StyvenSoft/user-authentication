130c1bd181642adc434d825ee4b748f195fea27b
Steveen <steveen.echeverri@gmail.com>
2020-10-08 16:46:35 -0500

    Intance input useState form register

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,7 +1,8 @@
 import { Container } from '@material-ui/core'
-import React from 'react'
+import React,  { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,20 +26,51 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Register() {
     const classes = useStyles();
+    const [displayName, setDisplayName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [passwordCheck, setPasswordCheck] = useState();
+    
+    const submit = async (e) => {
+        e.preventDefault();
+    }
 
     return (
         <div>
             <Container maxWidth="xs">
                 <h2>Register</h2>
-                <form className={classes.root} noValidate autoComplete="off">
-                    <TextField id="standard-basic" label="Email" className={classes.textfield} />
+                <form className={classes.root} noValidate autoComplete="off" onSubmit={submit}>
+                    <TextField 
+                        id="standard-basic" 
+                        label="Name Full" 
+                        className={classes.textfield}
+                        onChange={(e) => setDisplayName(e.target.value)} 
+                    />
+                    <TextField 
+                        id="standard-basic" 
+                        label="Email" 
+                        className={classes.textfield} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
                     <TextField
                         id="standard-password-input"
                         label="Password"
                         type="password"
                         autoComplete="current-password"
                         className={classes.textfield}
+                        onChange={(e) => setPassword(e.target.value)} 
                     />
+                    <TextField
+                        id="standard-password-input"
+                        label="Verify password"
+                        type="password"
+                        autoComplete="current-password"
+                        className={classes.textfield}
+                        onChange={(e) => setPasswordCheck(e.target.value)} 
+                    />
+                    <Button variant="contained" style={{width: '80%', marginTop: '30px'}}>
+                        Register
+                    </Button>
                 </form>
             </Container>
         </div>
