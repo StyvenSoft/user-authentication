dffde16659dc17800f50f83ac7002548cb1ab54e
Steveen <steveen.echeverri@gmail.com>
2020-10-08 11:57:59 -0500

    Change style component TextField white

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -10,6 +10,17 @@ const useStyles = makeStyles((theme) => ({
             width: '25ch',
         },
     },
+    textfield: {
+        "& .MuiFormLabel-root, & .MuiInputBase-root": {
+            color: "white" 
+        },
+        "& .MuiInput-underline:before": {
+            borderBottom: "1px solid rgb(226 219 219 / 42%)"
+        },
+        "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+            borderBottom: "2px solid rgb(226 219 219 / 42%)"
+        }
+    }
 }));
 
 export default function Register() {
@@ -20,12 +31,13 @@ export default function Register() {
             <Container maxWidth="xs">
                 <h2>Register</h2>
                 <form className={classes.root} noValidate autoComplete="off">
-                    <TextField id="standard-basic" label="Email" />
+                    <TextField id="standard-basic" label="Email" className={classes.textfield} />
                     <TextField
                         id="standard-password-input"
                         label="Password"
                         type="password"
                         autoComplete="current-password"
+                        className={classes.textfield}
                     />
                 </form>
             </Container>
