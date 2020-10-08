2c4a5eef08ec80ec24623c0e432627a1056af2bf
Steveen <steveen.echeverri@gmail.com>
2020-10-07 19:46:49 -0500

    Register form input makeStyles

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,9 +1,34 @@
+import { Container } from '@material-ui/core'
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
 
 export default function Register() {
+    const classes = useStyles();
+
     return (
         <div>
-            Register
+            <Container maxWidth="xs">
+                <h2>Register</h2>
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField id="standard-basic" label="Email" />
+                    <TextField
+                        id="standard-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                    />
+                </form>
+            </Container>
         </div>
     )
 }
